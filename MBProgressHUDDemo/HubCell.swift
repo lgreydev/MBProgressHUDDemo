0b11f20b029967a097e82c2573af428c700b8c59
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-12 14:27:00 +0300

    Add TinyConstraints

--- MBProgressHUDDemo/HubCell.swift
+++ MBProgressHUDDemo/HubCell.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import TinyConstraints
 
 class HubCell: UICollectionViewCell {
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .magenta
+        setupView()
+
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension HubCell {
+    private func setupView() {
+        addSubview(titleLabel)
+        titleLabel.centerInSuperview()
     }
 }
