4c1327bf5ae0515e1f8757e5ac0728f6a6862358
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-12 12:49:56 +0300

    Add collection view

---
+++ MBProgressHUDDemo/HudsController.swift
@@ -0,0 +1,47 @@
+//
+//  HudsController.swift
+//  MBProgressHUDDemo
+//
+//  Created by Sergey Lukaschuk on 12.05.2022.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class HudsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 3
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
