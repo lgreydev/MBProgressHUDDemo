5eada69e2a96d4182eba7a469afefa92ef686a33
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-05-12 15:26:25 +0300

    Add items

--- MBProgressHUDDemo/Hud.swift
+++ MBProgressHUDDemo/Hud.swift
@@ -14,10 +14,121 @@ class Hud {
 
     enum HudAction {
         case indeterminateExample
+        case labelExample
+        case detailsLabelExample
+        case determinateExample
+        case annularDeterminateExample
+        case barDeterminateExample
+        case textExample
+        case customViewExample
+        case cancelationExample
+        case modeSwitchingExample
+        case windowExample
+        case networkingExample
+        case determinateNSProgressExample
+        case dimBackgroundExample
+        case colorExample
     }
 
     init(title: String, action: HudAction) {
         self.title = title
         self.action = action
+    }
+}
+
+extension Hud {
+    func runHub(action: HudAction) {
+        switch action {
+        case .indeterminateExample:
+            indeterminateExample()
+        case .labelExample:
+            labelExample()
+        case .detailsLabelExample:
+            detailsLabelExample()
+        case .determinateExample:
+            determinateExample()
+        case .annularDeterminateExample:
+            annularDeterminateExample()
+        case .barDeterminateExample:
+            barDeterminateExample()
+        case .textExample:
+            textExample()
+        case .customViewExample:
+            customViewExample()
+        case .cancelationExample:
+            cancelationExample()
+        case .modeSwitchingExample:
+            modeSwitchingExample()
+        case .windowExample:
+            windowExample()
+        case .networkingExample:
+            networkingExample()
+        case .determinateNSProgressExample:
+            determinateNSProgressExample()
+        case .dimBackgroundExample:
+            dimBackgroundExample()
+        case .colorExample:
+            colorExample()
+        }
+    }
+
+    private func indeterminateExample() {
+        print(#function)
+    }
+
+    private func labelExample() {
+        print(#function)
+    }
+
+    private func detailsLabelExample() {
+        print(#function)
+    }
+
+    private func determinateExample() {
+        print(#function)
+    }
+
+    private func annularDeterminateExample() {
+        print(#function)
+    }
+
+    private func barDeterminateExample() {
+        print(#function)
+    }
+
+    private func textExample() {
+        print(#function)
+    }
+
+    private func customViewExample() {
+        print(#function)
+    }
+
+    private func cancelationExample() {
+        print(#function)
+    }
+
+    private func modeSwitchingExample() {
+        print(#function)
+    }
+
+    private func windowExample() {
+        print(#function)
+    }
+
+    private func networkingExample() {
+        print(#function)
+    }
+
+    private func determinateNSProgressExample() {
+        print(#function)
+    }
+
+    private func dimBackgroundExample() {
+        print(#function)
+    }
+
+    private func colorExample() {
+        print(#function)
     }
 }
--- MBProgressHUDDemo/HudsController.swift
+++ MBProgressHUDDemo/HudsController.swift
@@ -12,9 +12,28 @@ private let reuseIdentifier = "Cell"
 class HudsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     let hud00 = Hud(title: "Indeterminate mode", action: .indeterminateExample)
+    let hud01 = Hud(title: "With label", action: .labelExample)
+    let hud02 = Hud(title: "With details label", action: .detailsLabelExample)
+    let hud10 = Hud(title: "Determinate mode", action: .determinateExample)
+    let hud11 = Hud(title: "Annular determinate mode", action: .annularDeterminateExample)
+    let hud12 = Hud(title: "Bar determinate mode", action: .barDeterminateExample)
+    let hud20 = Hud(title: "Text only", action: .textExample)
+    let hud21 = Hud(title: "Custom view", action: .customViewExample)
+    let hud22 = Hud(title: "With action button", action: .cancelationExample)
+    let hud23 = Hud(title: "Mode switching", action: .modeSwitchingExample)
+    let hud24 = Hud(title: "On window", action: .windowExample)
+    let hud30 = Hud(title: "NSURLSession", action: .networkingExample)
+    let hud31 = Hud(title: "Determinate with NSProgress", action: .determinateNSProgressExample)
+    let hud32 = Hud(title: "Dim background", action: .dimBackgroundExample)
+    let hud33 = Hud(title: "Colored", action: .colorExample)
 
     lazy var huds: [[Hud]] = {
-        let huds = [[hud00]]
+        let huds = [
+            [hud00, hud01, hud02],
+            [hud10, hud11, hud12],
+            [hud20, hud21, hud22, hud23, hud24],
+            [hud30, hud31, hud32, hud33]
+        ]
         return huds
     }()
 
@@ -44,10 +63,19 @@ extension HudsController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("tap")
+        huds[indexPath.section][indexPath.row].runHub(action: .indeterminateExample)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 50)
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 50)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2.0
     }
 }
